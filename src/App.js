0280feb6d61eab1f44a29190cb17e66478464d59
Kanchan Jadhav <kanchanjadhav0223@gmail.com>
2023-05-28 13:55:32 +0530

    toggle background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+ import './App.css';
+// // import App from './component/App';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
+// export default App;
+
+import React, { useState } from 'react';
+
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState('white');
+
+  const toggleBackgroundColor = () => {
+    setBackgroundColor(prevColor => prevColor === 'white' ? 'lightgray' : 'lightblue' );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='toggle'>
+    <div style={{ backgroundColor: backgroundColor, height: '100vh' }}>
+      <button onClick={toggleBackgroundColor}>Toggle Background Color</button>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
+
